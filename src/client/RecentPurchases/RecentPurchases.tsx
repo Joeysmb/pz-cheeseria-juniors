df10f45c59dc1b8128655df7485ef0359ad819a6
Joseph Nwafor <joeysmb@rocketmail.com>
2021-06-28 00:04:13 +1000

    Create the RecentPurchases component and its corresponding styles

---
+++ src/client/RecentPurchases/RecentPurchases.tsx
@@ -0,0 +1,26 @@
+import { Wrapper } from './RecentPurchases.styles';
+import { CartItemType } from '../App';
+import RecentlyPurchasedItem from "./RecentlyPurchasedItem/RecentlyPurchasedItem"
+
+type Props = {
+    recentPurchases: CartItemType[]
+}
+
+const RecentPurchases: React.FC<Props> = ({recentPurchases}) => {
+
+    return(
+        <Wrapper>
+      <h2>Most recent purchases</h2>
+      {recentPurchases.length === 0 ? <p>No Purchases made yet.</p> : null}
+      {recentPurchases.map(item => (
+        <RecentlyPurchasedItem
+          key={item.id}
+          recentlyPurchasedItem={item}
+        />
+      ))}
+    </Wrapper>
+    );
+
+};
+
+export default RecentPurchases;
