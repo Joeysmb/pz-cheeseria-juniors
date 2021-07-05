039747375841734b412fa9a5fea6c3392fc1b2d0
Joseph Nwafor <joeysmb@rocketmail.com>
2021-07-05 13:06:23 +1000

    Update the server route api and also change how recent purchases are processed and displayed

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import {readFile, writeFile} from "fs";
 const cheeses = require('./data/cheeses.json');
 
+//Setting the type for each purchased item
 type item = {
     id: number;
     title: string;
@@ -13,34 +14,9 @@ type item = {
     amount?: number;
 };
 
+//Setting the type for the purchased item collection
 type itemArr = item[];
-
-// const newItems = [
-//     {
-//         "id": 1,
-//         "title": "ABBAYE DE BELLOC",
-//         "price": 109.95,
-//         "description": "Abbaye de Belloc is a flat wheel-shaped traditional, farmhouse, unpasteurised, semi-hard cheese made from sheep's milk. It has a natural, crusty, brownish rind with patches of red, orange and yellow. The rind is marked with tiny craters.",
-//         "category": "creamy, dense and firm",
-//         "image": "https://www.cheese.com/media/img/cheese/Abbaye-de-Belloc.jpg"
-//     },
-//     {
-//         "id": 2,
-//         "title": "ABBAYE DU MONT DES CATS",
-//         "price": 29.21,
-//         "description": "The Abbaye du Mont des Cats cheese is made by monks in a monastery of the same name in the town of Godewaersvelde, in Northern France. Cow's milk from local farms is used and the milk is gently pasteurised for cheese production. The maturation process takes about 4 to 5 weeks",
-//         "category": "semi-soft, artisan, brined",
-//         "image": "https://www.cheese.com/media/img/cheese/Mont_des_Cats_kaas.jpg"
-//     },
-//     {
-//         "id": 3,
-//         "title": "ADELOST",
-//         "price": 367.55,
-//         "description": "Adelost is a Swedish blue cheese made from cow's milk. The blue-grey veins running throughout are a distinctive feature of the cheese. It has a sharp, salty and tangy flavour. The ripening process is for two to three months. The cheese comes in a drum shape with a rind of pale cream, which is lightly dotted with moulds.",
-//         "category": "semi-soft, blue-veined",
-//         "image": "https://www.cheese.com/media/img/cheese/Adelost_QnxYLx6.jpg"
-//     }
-// ]
+type purchasesArr = [item[]];
 
 const app = express();
 app.use(express.static("./public"));
@@ -55,62 +31,78 @@ app.get('/api/cheeses', (req, res, next) => {
 
 app.post("/api/storeCheckout", (req, res, next) => {
     let newItems: itemArr = req.body;
+
+    //Get the stored items
     readFile('./src/server/data/recentPurchases.json', "utf-8", (err, oldItems)=>{
         if(err){
+            res.send("Checkout was not successful")
             console.log("Unable to read file - ERROR: " + err);
             return err
         }
-        try {
-            let OldItemsjson: itemArr = JSON.parse(oldItems);
-
+            //Convert retrieved items to JSON
+            let OldPurchasesjson: purchasesArr = JSON.parse(oldItems);
+             
+            let itemsWithoutAmount : itemArr = [];
             for(let i=0; i<newItems.length; i++){
-                let wasPurchasedRecently = false;
-                for(let j=0; j<OldItemsjson.length; j++){
-                    if(newItems[i].id === OldItemsjson[j].id){
-                        wasPurchasedRecently = true;
-                        break;
-                    }
-                }
-                wasPurchasedRecently ? null : OldItemsjson.push(newItems[i])
+                let obj = newItems[i];
+                itemsWithoutAmount.push({
+                    "id": obj.id, 
+                    "title": obj.title, 
+                    "price": obj.price, 
+                    "description": obj.description, 
+                    "category": obj.category, 
+                    "image": obj.image
+                })
             }
-
-            const allPurchasedItemsjson = [...OldItemsjson];
+            //Storing newer items at the on top of the list to ensure last in first out
+            OldPurchasesjson.unshift(itemsWithoutAmount)
+            const allPurchasedItemsjson = [...OldPurchasesjson];
             let allPurchasedItemsString = JSON.stringify(allPurchasedItemsjson, null, 2)
 
             writeFile('./src/server/data/recentPurchases.json', allPurchasedItemsString, (err)=>{
                 if(err){
+                    res.json({
+                        status: "Checkout was not successful",
+                        message: err
+                    })
                     console.log("Unable to read file - ERROR: " + err);
+                    return err
                 }else{
-                    console.log("File wrote successfully");
-                }
-            })
-        } catch (error) {
-            console.log("An Error occured ")
-        }
-    });
+                    res.json({
+                        status: "Checkout was Successful",
+                        message: "Thank you for shopping Cheeseria cheese"
+                    })
+                } 
+            }) //writefile
+        }); //readfile
 
-    console.log(" Post Request just hit the server")
-    console.log(req.body)
-    res.json({
-        status: "Successful",
-        Message: "Thank you for shopping Cheeseria cheese"
-    })
 });
 
 app.get("/api/recentlyPurchasedItems", (req, res, next) => {
-    console.log("Request received nanana")
     readFile('./src/server/data/recentPurchases.json', "utf-8", (err, data)=>{
         if(err){
             console.log(err);
             return res.json({"error": "Something went wrong. " + err});
         }
-        try {
-            const recentlyPurchasedItems = JSON.parse(data);
-            console.log(recentlyPurchasedItems);
+
+            const oldPurchasedItems: purchasesArr = JSON.parse(data);
+            var recentlyPurchasedItems: itemArr = []; //Container for most recent items
+            //Iterate through the previous orders
+                for(let i=0; i<oldPurchasedItems.length; i++){
+                    let order = oldPurchasedItems[i];
+                  for(let j=0; j<order.length; j++){
+                      //Is this item is already in the list of recently purchased items?
+                      if(JSON.stringify(recentlyPurchasedItems).includes(JSON.stringify(order[j]))){
+                          continue;
+                          //Return only the 5 most recent items 
+                      }else if(recentlyPurchasedItems.length >= 5){
+                        return res.json(recentlyPurchasedItems);
+                      }
+                      recentlyPurchasedItems.push(order[j]);
+                  }
+              }
+
             return res.json(recentlyPurchasedItems);
-        } catch (error) {
-            return res.json({"error": "Something went wrong. " + error});
-        }
     });
 });
 export default app;
